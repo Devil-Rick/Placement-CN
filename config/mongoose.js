7c02dfd48c04ae8186bcc1dc36e1527b9ff6d389
Devil-Rick <saptarshinaruto@gmail.com>
2024-01-25 18:29:58 +0530

    1st commit

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const DB = process.env.MONGODB_URL
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = mongoose;
